881a41f113dee3cb07f4d37a9c49b5ecd220cd58
rndeshpande <rahulnd@gmail.com>
2017-10-04 12:45:30 -0700

    Added toolbar to home screen

--- src/Twitter/app/src/main/java/com/codepath/apps/twitter/activities/HomeActivity.java
+++ src/Twitter/app/src/main/java/com/codepath/apps/twitter/activities/HomeActivity.java
@@ -1,14 +1,21 @@
 package com.codepath.apps.twitter.activities;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.codepath.apps.twitter.R;
 import com.codepath.apps.twitter.adapters.HomeFragmentPagerAdapter;
+import com.codepath.apps.twitter.utils.TwitterClient;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private TwitterClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,35 @@ public class HomeActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
+        setToolbar();
+    }
+
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                logout();
+                break;
+        }
+        return true;
+    }
+
+    private void logout() {
+        client.clearAccessToken();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
